0e54e424332dc151861e6ca2d483ce676b5e0346
fredo09 <fred_15@outlook.com>
2025-09-24 01:50:28 -0600

    Agregar manejo de excepciones en el servicio de productos y mejorar la creación de productos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { 
+  Logger,
+  Injectable, 
+  BadRequestException, 
+  InternalServerErrorException, 
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+import { Product } from './entities/product.entity';
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  private readonly logger = new Logger('ProductsService');
+
+  //* Ocupamos usar el patron repository para trabajar con la base de datos
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const savedProduct = this.productsRepository.create(createProductDto);
+      await this.productsRepository.save(savedProduct);
+
+      return savedProduct;
+    } catch (error) {
+      this._handleExceptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +45,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private _handleExceptions(error: any) {
+    if (error.code === '23505') 
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
   }
 }
