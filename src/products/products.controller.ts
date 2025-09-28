e563aa798888d3b9f9c6096a9b44f97d02600eb5
fredo09 <fred_15@outlook.com>
2025-09-28 16:23:24 -0600

    Agregar documentación del controlador de productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,6 +3,16 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+/**
+ * Controlador de productos
+ * Maneja las rutas y las solicitudes HTTP relacionadas con los productos
+ * Utiliza el servicio de productos para realizar operaciones CRUD
+ * Define rutas para crear, obtener, actualizar y eliminar productos
+ * Utiliza DTOs para validar y transformar los datos de entrada
+ * Incorpora manejo de excepciones para errores comunes
+ * @author fredy_vazzqz
+ * @version 1.0.0
+ */
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
