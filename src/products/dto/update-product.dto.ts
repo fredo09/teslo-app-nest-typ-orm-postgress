62368d8022c3f998f78e14105c87d157c52e3ed5
fredo09 <fred_15@outlook.com>
2025-10-06 22:16:27 -0600

    Agregar documentación y metadatos en el DTO de actualización de producto

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
+/**
+ * DTO para actualizar un producto
+ * Extiende de CreateProductDto para reutilizar las validaciones
+ * Todas las propiedades son opcionales para permitir actualizaciones parciales
+ * @author fredy_vazzqz
+ * @version 1.0.0
+ */
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
