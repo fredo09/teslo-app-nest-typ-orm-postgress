4bc9d947e9c1804753adcd409014cacbf23b72e7
fredo09 <fred_15@outlook.com>
2025-10-09 01:02:50 -0600

    Agregar entidad ProductImage y actualizar módulo de productos para incluirla

---
+++ src/products/entities/product-image.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+/**
+ * ProductImage entity representing an image associated with a product.	
+ * This class can be expanded with properties such as URL, alt text, and metadata as needed.
+ * @entity
+ * @table product_images
+ * @author alfredo_vazquez
+ * @date 2024-06-10
+ * @version 1.0.0
+ */
+
+@Entity()
+export class ProductImage {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column('text')
+	url: string;
+}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// * SERVICES AND CONTROLLERS
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './entities/product.entity';
+// * ENTITIES
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   //* Importamos todos los entities para typeorm
   imports: [TypeOrmModule.forFeature([
-    //Entities ORM
-    Product
+    // * Entities ORM -> Poner cada entidad que se cree y se vaya a usar en el modulo
+    Product,
+    ProductImage
   ])],
 })
 export class ProductsModule {}
