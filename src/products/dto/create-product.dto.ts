f24faa2ee952487a296281dcfbb16797b721e607
fredo09 <fred_15@outlook.com>
2025-10-13 22:05:41 -0600

    Agregar documentación para el DTO de creación de productos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -9,6 +9,14 @@ import {
 	MinLength } 
 from "class-validator";
 
+/**
+ * DTO para crear un producto
+ * Define y valida la estructura de los datos necesarios para crear un producto
+ * Utiliza decoradores de class-validator para asegurar que los datos cumplen con los requisitos
+ * Incluye propiedades como título, precio, descripción, slug, stock, tallas, género, etiquetas e imágenes
+ * @author fredy_vazzqz
+ * @version 1.0.0
+ */
 //* informacion que se necesita para insertar un producto a la base de datos
 export class CreateProductDto {
 	@IsString()
