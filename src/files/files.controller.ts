f02de3c6a6166fdd520f019e4881979250ff1357
fredo09 <fred_15@outlook.com>
2025-10-19 21:07:16 -0600

    feat: agregar soporte para la carga de imágenes de productos en el controlador de archivos

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
 import { FilesService } from './files.service';
+
+
+/**
+ * Controlador para la gestión de archivos.
+ * Proporciona endpoints para subir y manejar archivos.
+ * Utiliza el servicio FilesService para la lógica de negocio relacionada con archivos.
+ * @author Alfredo Vázquez
+ * @version 1.0.0
+ */
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file')) // 'file' es el nombre del campo en el formulario que contiene el archivo
+  uploadProductImageFile(
+    @UploadedFile() file: Express.Multer.File, // 'Indica el tipado de file ' Express.Multer.File
+  ) {
+    
+
+    return file;
+  }
 }
