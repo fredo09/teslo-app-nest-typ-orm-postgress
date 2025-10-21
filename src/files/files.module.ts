d985c69ea9a28d327b8ecb8602454ec5f79a54f5
fredo09 <fred_15@outlook.com>
2025-10-21 14:21:31 -0600

    feat: agregar documentación para el módulo y servicio de gestión de archivos

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -2,6 +2,13 @@ import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
 
+/**
+ * Módulo para la gestión de archivos.
+ * Agrupa el controlador y el servicio relacionados con archivos.
+ * @author Alfredo Vázquez
+ * @version 1.0.0
+ */
+
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+/**
+ * Servicio para la gestión de archivos.
+ * Proporciona métodos para manejar la lógica de negocio relacionada con archivos.
+ * @author Alfredo Vázquez
+ * @version 1.0.0
+ */
 @Injectable()
 export class FilesService {
 }
