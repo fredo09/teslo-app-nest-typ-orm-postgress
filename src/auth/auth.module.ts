e00b75bc6a80551a6df2003c0c71cd30b465cdc0
fredo09 <fred_15@outlook.com>
2025-11-04 19:43:06 -0600

    feat: agregar entidad User y configurar módulo de autenticación

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+import { User } from './entities/user.entity';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    TypeOrmModule.forFeature([ //* Nos permite importar las entidades y hacer tablas en DB
+      User
+    ])
+  ]
 })
 export class AuthModule {}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('users')
+export class User {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column('text', {
+		unique: true,
+
+	})
+	email: string;
+
+	@Column('text')
+	password: string;
+
+	@Column({
+		type: 'text',
+		nullable: true,
+	})
+	fullName: string;
+
+	@Column('bool')
+	isActive: boolean;
+
+	@Column("text", {
+		array: true,
+		default: ['user'], // * por defecto todos los usuarios son 'user' (rol basico)
+	})
+	roles: string[];
+
+
+}
