38dbb709f9520534141d422219f3c1b7586496d8
fredo09 <fred_15@outlook.com>
2025-11-09 23:11:59 -0600

    feat: agregar métodos hashSync y compareSync a la interfaz BcryptAdapterI y su implementación

--- src/common/adapter/bcrypt.adatper.ts
+++ src/common/adapter/bcrypt.adatper.ts
@@ -14,7 +14,23 @@ import { BcryptAdapterI } from "../interface/bcrypt.interface";
 export class BcryptAdapter implements BcryptAdapterI {
 	private bcrypt: typeof bcrypt = bcrypt;
 
+	/**
+	 * Genera un hash para la contraseña proporcionada
+	 * @param password la contraseña a hashear
+	 * @param saltRounds el número de rondas de sal (opcional, por defecto es 10)
+	 * @returns el hash de la contraseña
+	 */
 	hashSync(password: string, saltRounds: number = 10): string {
 		return this.bcrypt.hashSync(password, saltRounds);
+	}
+
+	/**
+	 * Compara una contraseña en texto plano con un hash
+	 * @param password la contraseña a comparar
+	 * @param hash el hash contra el que se va a comparar
+	 * @returns true si las contraseñas coinciden, false en caso contrario
+	 */
+	compareSync(password: string, hash: string): boolean {
+		return this.bcrypt.compareSync(password, hash);
 	}
 }
--- src/common/interface/bcrypt.interface.ts
+++ src/common/interface/bcrypt.interface.ts
@@ -5,5 +5,17 @@
  * and returns a Promise that resolves to any type of data.
  */
 export interface BcryptAdapterI {
+	/**
+	 * Genera un hash para la contraseña proporcionada
+	 * @param password la contraseña a hashear
+	 * @param saltRounds el número de rondas de sal (opcional, por defecto es 10)
+	 */
 	hashSync(password: string, saltRounds: number): string;
+
+	/**
+	 * Compara una contraseña en texto plano con un hash
+	 * @param password la contraseña a comparar
+	 * @param hash el hash contra el que se va a comparar
+	 */
+	compareSync(password: string, hash: string): boolean;
 }
