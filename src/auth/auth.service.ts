b59fb0817736b192a6769c73182d2f041dd1fcb4
fredo09 <fred_15@outlook.com>
2025-11-24 23:22:26 -0600

    feat: eliminar importación innecesaria de 'find' en AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,6 @@ import { BadRequestException, Injectable, Logger, UnauthorizedException } from '
 import { User } from './entities/user.entity';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { BcryptAdapter } from './../common/adapter/bcrypt.adatper';
-import { find } from 'rxjs';
 
 /**
  * Servicio de autenticación
