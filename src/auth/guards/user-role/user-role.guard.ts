dd3672800033aa1c5d93c8c6ae5cac6b09e59376
fredo09 <fred_15@outlook.com>
2026-01-02 19:37:26 -0600

    feat: mejorar la guardia de rol de usuario para validar roles y manejar errores

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadGatewayException, CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 /**
  * Guardia de rol de usuario para proteger rutas basadas en roles.
@@ -16,9 +17,22 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     // * Obtener los roles válidos desde los metadatos de la ruta
-    const validRoles = this.reflector.get<string[]>('roles', context.getHandler());
-    console.log({ validRoles });
+    const validRoles: string[] = this.reflector.get<string[]>('roles', context.getHandler());
+    const { user } = context.switchToHttp().getRequest();
+    // const { user }: { user: User } = context.switchToHttp().getRequest();
 
-    return true;
+    if (!validRoles || validRoles.length === 0) return true;
+
+    if (!user)
+      throw new BadGatewayException('User not found in request');
+
+    for(const role of user.roles) {
+      if (validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new BadGatewayException(`User ${user.fullName} does not have valid roles`);
+    // return user.roles.some(role => validRoles.includes(role));
   }
 }
