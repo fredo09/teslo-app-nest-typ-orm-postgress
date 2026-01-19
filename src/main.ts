1445021225c6ef52886b2bd67005084ec6b5f79b
fredo09 <fred_15@outlook.com>
2026-01-18 23:19:14 -0600

    feat: reorganizar importaciones y agregar configuración de Swagger para la documentación de la API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+
+//* DOCUMENTACION API 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +17,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo Shop Api')
+    .setDescription('The Documentation Api - Nest')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`🚀 ~ lanzado el servidor en el puerto ${process.env.PORT ?? 3000}`);
