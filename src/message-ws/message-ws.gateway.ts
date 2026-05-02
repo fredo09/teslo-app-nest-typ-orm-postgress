b71e73b87d624aef8f697b7c154ddf8c819579de
fredo09 <fred_15@outlook.com>
2026-05-02 02:03:07 -0600

    feat: mejorar manejo de conexiones y desconexiones en WebSocket, incluyendo emisión de actualizaciones a clientes conectados y agregando cometarios al codigo

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,24 +1,53 @@
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
-import { OnGatewayConnection, OnGatewayDisconnect , WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect , WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 
+/**
+ * Message WebSocket Gateway
+ * This gateway handles WebSocket connections for messaging functionality.
+ * It allows clients to connect and disconnect, and keeps track of connected clients.
+ * It also emits updates to all clients when the number of connected clients changes.
+ * 
+ * @author @alfredVazquez
+ * @version 1.0.0
+ * @see https://docs.nestjs.com/websockets/gateways
+ * @see https://socket.io/docs/v4/
+ */
 @WebSocketGateway({ cors: true })
 export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer() wss!: Server;
+  
   constructor(
     private readonly messageWsService: MessageWsService
   ) {}
 
+  /**
+   * Handles the connection of a client
+   * @param client {Socket}
+   */
   handleConnection(client: Socket) {
     this.messageWsService.registerClient(client);
     console.log("🚀 ~ Clientes conectados : ", {
       CountClients: this.handleClientsConnected() 
     });
+    this.wss.emit('clients-updated', this.handleClientsConnected());
   }
 
+  /**
+   * Handles the disconnection of a client
+   * @param client {Socket}
+   */
   handleDisconnect(client: Socket) {
     this.messageWsService.removeClient(client.id);
+    console.log("🚀 ~ Clientes conectados : ", {
+      CountClients: this.handleClientsConnected() 
+    });
   }
 
+  /**
+   * returns the list of connected clients
+   * @returns 
+   */
   handleClientsConnected() {
     return this.messageWsService.getConnectedClients();
   }
--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,6 +1,15 @@
 import { Socket } from 'socket.io';
 import { Injectable } from '@nestjs/common';
 
+/**
+ * ConnectedClients interface
+ * This interface defines the structure of the connected clients object.
+ * It uses the client ID as the key and the Socket object as the value.
+ * 
+ * @author @alfredVazquez
+ * @version 1.0.02
+ * @see https://socket.io/docs/v4/server-socket-instance/
+ */
 interface ConnectedClients {
 	[id: string]: Socket;
 }
@@ -9,15 +18,27 @@ interface ConnectedClients {
 export class MessageWsService {
 	private connectedClients: ConnectedClients = {};
 
+	/**
+	 * Registers a client as connected
+	 * @param client {Socket}
+	 */
 	registerClient(client: Socket) {
 		this.connectedClients[client.id] = client;
 	}
 
+	/**
+	 * Removes a client from the list of connected clients
+	 * @param clientId {string}
+	 */
 	removeClient(clientId: string) {
 		delete this.connectedClients[clientId];
 	}
 
-	getConnectedClients(): number {
-		return Object.keys(this.connectedClients).length;
+	/**
+	 * Returns the list of connected clients
+	 * @returns {string[]}
+	 */
+	getConnectedClients(): string[] {
+		return Object.keys(this.connectedClients);
 	}
 }
